59d7033406f709582fc1ec2855b84830adf1b464
Débora Santos Gaspar <deborasgaspar21@gmail.com>
2023-02-13 18:44:52 -0300

    funcoes agregadas

---
+++ funcoes_agregadas/SQLQuery2.sql
@@ -0,0 +1,21 @@
+USE Northwind;
+
+--MIN(), MAX(), COUNT(*), SUM(), AVG(), HAVING
+--Visualizando as tabelas:
+
+SELECT * FROM Categories;
+SELECT * FROM CustomerCustomerDemo;
+SELECT * FROM CustomerDemographics;
+SELECT * FROM Customers;
+SELECT * FROM Employees;
+SELECT * FROM EmployeeTerritories;
+SELECT * FROM [Order Details];
+SELECT * FROM Orders;
+SELECT * FROM Products;
+SELECT * FROM Region;
+SELECT * FROM Shippers;
+SELECT * FROM Suppliers;
+SELECT * FROM Territories;
+
+
+--MIN(), MAX(), COUNT(*), SUM(), AVG(), HAVING
